e0c8f044ee2690594881d1af4badba99653a5d9c
kluhini96 <kieranhalliday@Kierans-MacBook-Pro.local>
2018-02-20 08:50:07 -0800

    First book completely downloaded

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import wget
 
 with open('allit.html') as fp:
-    soup = BeautifulSoup(fp, 'lxml')
-
     browser = webdriver.Safari();
-
     browser.get("http://allitebooks.com/illustrated-c-7-5th-edition")
 
-    print(browser.find_elements_by_class_name('download-links'))
+    elem = browser.find_elements_by_class_name("download-links")[0]
+    wget.download(elem.find_element_by_css_selector('a').get_attribute('href'))
 
-
-    browser.find_elements_by_class_name("download-links")[0].click()
+    
     
