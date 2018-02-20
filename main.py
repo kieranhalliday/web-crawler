634961e2f850bde3717b1c70d4b0bc33e95e6ee0
Kieran Halliday <kluhini96@hotmail.com>
2018-02-20 11:25:23 -0800

    Got every book from the first page of all the side bars

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ import wget
 
 with open('allit.html') as fp:
     browser = webdriver.Safari();
-
-    browser.get("http://www.allitebooks.com")
+    nextURL = "http://www.allitebooks.com"
+    browser.get(nextURL)
+    
     main_content = browser.find_element_by_id('main-content')
-    element_list = main_content.find_elements_by_tag_name('article')
+    main_element_list = main_content.find_elements_by_tag_name('article')
+        
+    side_content = browser.find_element_by_id('side-content')
+    side_element_list = side_content.find_elements_by_tag_name('li')
 
 
-    for x in range(0, len(element_list)):
-        browser.get(element_list[x].find_element_by_class_name('entry-thumbnail hover-thumb').find_element_by_css_selector('a').get_attribute('href'))
-        elem = browser.find_elements_by_class_name("download-links")[0]
-        print(elem.find_element_by_css_selector('a').get_attribute('href'))
-        wget.download(elem.find_element_by_css_selector('a').get_attribute('href'))
-        browser.get("http://www.allitebooks.com")
-        main_content = browser.find_element_by_id('main-content')
-        element_list = main_content.find_elements_by_tag_name('article')
+    for y in range(0,len(side_element_list)):
+        for x in range(0, len(main_element_list)):
+            browser.get(main_element_list[x].find_element_by_class_name('entry-thumbnail hover-thumb').find_element_by_css_selector('a').get_attribute('href'))
+            elem = browser.find_elements_by_class_name("download-links")[0]
+            print(elem.find_element_by_css_selector('a').get_attribute('href'))
+            wget.download(elem.find_element_by_css_selector('a').get_attribute('href'))
 
-    #How to get a single books from a pre-known webpage
-    #browser.get("http://allitebooks.com/illustrated-c-7-5th-edition")
-
-    #elem = browser.find_elements_by_class_name("download-links")[0]
-    #wget.download(elem.find_element_by_css_selector('a').get_attribute('href'))
-
+            # Reset loop variables
+            browser.get(nextURL)
+            main_content = browser.find_element_by_id('main-content')
+            main_element_list = main_content.find_elements_by_tag_name('article')
+            
+        side_content = browser.find_element_by_id('side-content')
+        side_element_list = side_content.find_elements_by_tag_name('li')
+        nextURL = side_element_list[y].find_element_by_css_selector('a').get_attribute('href')
+        print(nextURL)
     
     
