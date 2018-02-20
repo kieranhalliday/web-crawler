e09c613a60bda7e6a89fcd37d6f62db6c89118c8
Kieran Halliday <kluhini96@hotmail.com>
2018-02-20 10:53:37 -0800

    First page done

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ with open('allit.html') as fp:
     browser.get("http://www.allitebooks.com")
     main_content = browser.find_element_by_id('main-content')
     element_list = main_content.find_elements_by_tag_name('article')
-    print(element_list)
-
-    print(element_list[0].find_element_by_class_name('entry-thumbnail hover-thumb').find_element_by_css_selector('a').get_attribute('href'))
 
 
-
+    for x in range(0, len(element_list)):
+        browser.get(element_list[x].find_element_by_class_name('entry-thumbnail hover-thumb').find_element_by_css_selector('a').get_attribute('href'))
+        elem = browser.find_elements_by_class_name("download-links")[0]
+        print(elem.find_element_by_css_selector('a').get_attribute('href'))
+        wget.download(elem.find_element_by_css_selector('a').get_attribute('href'))
+        browser.get("http://www.allitebooks.com")
+        main_content = browser.find_element_by_id('main-content')
+        element_list = main_content.find_elements_by_tag_name('article')
 
     #How to get a single books from a pre-known webpage
     #browser.get("http://allitebooks.com/illustrated-c-7-5th-edition")
