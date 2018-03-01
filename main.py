3bef2bc8a6b87f315d46c19fd766981122cfa7d3
Kieran Halliday <kluhini96@hotmail.com>
2018-03-01 10:34:50 -0800

    Commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open('allit.html') as fp:
     browser = webdriver.Safari();
     
     # Change this to the sidebar URL you want to start on
-    nextURL = "http://www.allitebooks.com/web-development/"
+    nextURL = "http://www.allitebooks.com/datebases/page/1/"
     browser.get(nextURL)
     
     main_content = browser.find_element_by_id('main-content')
@@ -15,14 +15,15 @@ with open('allit.html') as fp:
     side_content = browser.find_element_by_id('side-content')
     side_element_list = side_content.find_elements_by_tag_name('li')
 
-    for z in range(0,len(side_element_list)):
+    # The starting index is the second element on the side bar you iterate through
+    for z in range(2,len(side_element_list)):
         pageList = browser.find_element_by_class_name('pagination clearfix')
         print (pageList.text)
         numberOfPages = re.findall(r'\d{2,3}',pageList.text)
         print numberOfPages[0]
         
         # The starting index is the page you want to go to after executing loop once
-        for y in range (45,int(numberOfPages[0])):
+        for y in range (2,int(numberOfPages[-1])):
             for x in range(0, len(main_element_list)):
                 # Get books from current page
                 try:
@@ -39,8 +40,7 @@ with open('allit.html') as fp:
                 main_element_list = main_content.find_elements_by_tag_name('article')
 
             # Set next page of books to go to
-            nextURL = nextURL.rstrip('page/' + str(y-1) + '/')
-            nextURL = nextURL + "/page/" + str(y) + "/"
+            nextURL = re.sub(str(y-1), str(y), nextURL)
             print(nextURL)
             
         # Go to next sidebar
